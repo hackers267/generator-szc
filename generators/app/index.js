9bdacf822f86ca5e4c5b0d4ff7eec5b83a355cfe
silence_zhpf <silence_zhpf@aliyun.com>
2021-02-13 00:24:36 +0800

    feat(index): 添加模板问答

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,9 +1,26 @@
 const Generator = require("yeoman-generator");
 module.exports = class extends Generator {
+  async prompting() {
+    this.answer = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname,
+      },
+      {
+        type: "input",
+        name: "desc",
+        message: "Your project description",
+        default: "",
+      },
+    ]);
+  }
+
   writing() {
     const template = this.templatePath();
     const destination = this.destinationPath();
-    const context = { name: "temp" };
+    const context = { name: this.answer.name, desc: this.answer.desc };
     console.warn(template);
     this.fs.copyTpl(template, destination, context, {
       globOptions: { dot: true },
